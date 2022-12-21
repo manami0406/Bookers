6e75fcd10509341004cae5dbda3c0d706d4b975a
EC2 Default User <ec2-user@ip-172-31-21-126.ap-northeast-1.compute.internal>
2022-12-21 08:51:32 +0000

    [Add]Top画面の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,28 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
-
+  
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/top'
+    # 詳細画面へリダイレクトへ直す
+  end
+  
   def show
   end
 
   def edit
+  end
+  
+  private
+  # ストロングパラメーター
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
