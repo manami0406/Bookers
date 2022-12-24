94e8b6e86019c96a9bb5a258b6f878a896e0bc22
EC2 Default User <ec2-user@ip-172-31-21-126.ap-northeast-1.compute.internal>
2022-12-24 09:32:49 +0000

    [Add]リンクの作成、destroy機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.save
-    redirect_to '/books'
+    redirect_to show_book_path(@book.id)
     # 詳細画面へリダイレクトに直す
   end
   
@@ -15,6 +15,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to "/books/#{book.id}" 
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
   
   private
